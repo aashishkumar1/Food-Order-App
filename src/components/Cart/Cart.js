d047b4282416f80894ecf25a777e20915725d649
Aashish Kumar <91531431+aashishkumar1@users.noreply.github.com>
2022-07-19 18:34:57 +0530

    Checkout form is added with some basic validations.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,12 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import CartContext from '../../store/cart-context';
 import Modal from '../UI/Modal';
 import classes from "./Cart.module.css";
 import CartItem from './CartItem';
+import Checkout from './Checkout';
 
 const Cart = (props) => {
+  const [isCheckout,setIsCheckout] = useState(false);
   const cartCtx= useContext(CartContext);
   const cartItemRemoveHandler = id => {
     cartCtx.removeItem(id);
@@ -13,11 +15,20 @@ const Cart = (props) => {
     cartCtx.addItem({...item,amount:1});
   }
 
+  const orderHandler = () => {
+    setIsCheckout(true);
+  }
+
   const cartItems = <ul className={classes['cart-items']}>{cartCtx.items.map((item)=> (<CartItem key={item.id} onAdd={cartItemAddHandler.bind(null,item)} onRemove={cartItemRemoveHandler.bind(null,item.id)} name={item.name} amount={item.amount} price={item.price}/>))}</ul>
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
 
   const hasItems = cartCtx.items.length > 0;
+
+  const modalActions = (<div className={classes.actions}>
+    <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
+    { hasItems && <button className={classes.button} onClick={orderHandler}>Order</button>}
+  </div>);
 
   return (
     <Modal onClose={props.onClose}>
@@ -26,10 +37,8 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-        { hasItems && <button className={classes.button}>Order</button>}
-      </div>
+      {isCheckout && <Checkout onCancel={props.onClose}/>}
+      {!isCheckout && modalActions}
     </Modal>
   )
 }
