be56ce6b397b91d741e8f0fe15be302c63a931bf
Aashish Kumar <91531431+aashishkumar1@users.noreply.github.com>
2022-07-19 15:50:12 +0530

    Loading is added until data is fetched.

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,11 +7,19 @@ import MealItem from './MealItem/MealItem';
 
 const AvailableMeals = () => {
   const [meals,setMeals] = useState([]);
+  const [isLoading,setIsLoading] = useState(true); // for loading till meals are loading
+  const [error,setError] = useState(); // for error while fetch request
 
   useEffect(() => {
 
     const fetchMeals = async () => {
       let response = await fetch('https://react-auth-8349e-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json')
+      
+      if(!response.ok)
+      {
+        throw new Error('Something went wrong!');
+      }
+
       let data = await response.json();
 
       const loadedMeals = [];
@@ -25,10 +33,32 @@ const AvailableMeals = () => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     }
     
-    fetchMeals();
+      fetchMeals().catch(error =>{
+      setIsLoading(false);
+      setError(error.message);
+    });
+    
   },[]);
+
+  if(isLoading){
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading Meals...</p>
+      </section>
+    );
+  }
+
+  if(error)
+  {
+    return (
+      <section className={classes.MealsError}>
+        <p>{error}</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map(meal => <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price}/>);
 
